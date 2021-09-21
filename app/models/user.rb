84362170686ccb53119bc7def28190721d12a2ba
sekretskwirll <will.mooreston+sekret@gmail.com>
2021-09-21 14:43:35 -0400

    added email.downcase before validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :ratings
   validates :email, uniqueness: true
-  before_save :format_email # TODO: this should happen before the uniqueness check
+  before_validation { self.email = email.downcase }
+  before_save :format_email
 
   def format_email
     email.downcase!
